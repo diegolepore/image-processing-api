46a5ffda02be9c115d290b7369849dd8b8f1cee0
Diego Palacios <diegopalacios3@gmail.com>
2021-07-15 17:57:01 +0200

    feat: Add initial/basic image processing logic
    Import sharp in router module and implement
    initial/basic image processing logic

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,48 @@
 import express from 'express'
+import sharp from 'sharp'
+import fs from 'fs'
+import path from 'path'
 
 const images = express.Router()
 
 images.get('/', (req, res) => {
-  res.send('Main images route')
+
+  const filename = req.query.filename
+  const width = (req.query.width as unknown) as string
+  const height = (req.query.height as unknown) as string
+
+  if(filename && width && height) {
+    try {
+      const fullFilePath = `full/${filename}.jpeg`
+      const thumbFilePath = `thumb/${filename}.jpeg`
+
+      fs.stat(thumbFilePath, (error, stats)  => {
+        if (error) {
+          (async () => {
+            const image = await sharp(fullFilePath)
+              .resize({ width: parseInt(width), height: parseInt(height) })
+              .toBuffer()
+            fs.writeFileSync(thumbFilePath, image)
+            process.stdout.write(`
+              Image has been processed:
+              Stats: ${stats}
+            `)
+            res.status(200).sendFile(path.resolve(thumbFilePath))
+          })()
+        } else {
+          process.stdout.write('File already exist')
+          res.sendFile(path.resolve(thumbFilePath))
+        }
+      })
+    } catch (error) {
+      process.stdout.write(error)
+      res.status(500)
+    }
+
+  } else {
+    process.stdout.write('Image can\'t be processed')
+    res.send('Image can\'t be processed')
+  }
 })
 
 export default images
