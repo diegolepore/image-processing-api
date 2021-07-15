36bcb98c632bc1f4909689ef665f4d99089dee5a
Diego Palacios <diegopalacios3@gmail.com>
2021-07-16 01:27:33 +0200

    refactor: Clean and organize the middleware code
    Additionally, add make sure all error messages
    work correctly, among other style tweaks.

--- src/middleware/images.middleware.ts
+++ src/middleware/images.middleware.ts
@@ -3,48 +3,80 @@ import { promises as fsPromises } from 'fs'
 import fs from 'fs'
 import path from 'path'
 
-const imageProcessingMiddleware = async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+const inlineStyles = `position: absolute; left: 50%; 
+  top: 100px; transform: translateX(-50%);
+  font-family: sans-serif; text-align: center;
+  padding: 0 20px 10px 20px; background-color: #ffdd59; border-radius: 8px;`
 
-  const filename = req.query.filename
-  const width = (req.query.width as unknown) as string
-  const height = (req.query.height as unknown) as string
-  const sourceformat = req.query.sourceformat || 'jpeg'
-  const outputformat = req.query.outputformat || 'jpeg'
-
+const checkFullImageExists = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  const { filename, width, height } = req.query
+  
   if(filename && width && height) {
+    const sourceformat = req.query.sourceformat || 'jpeg'
     const fullFilePath = `full/${filename}.${sourceformat}`
-    const thumbFilePath = `thumb/${filename}-w${width}-h${height}.${outputformat}`
-    
-    try {
-      if (!fs.existsSync('thumb')) {
-        await fsPromises.mkdir('thumb')
-      }
-    } catch (error) {
-      process.stdout.write(error)
-      res.status(500)
-    }
-
     fs.stat(fullFilePath, (error)  => {
       if(error) {
-        const errorMsg = `File with sourceformat: ${sourceformat} don'n exist`
-        process.stdout.write(errorMsg)
+        const errorMsg = `<div style="${inlineStyles}">
+          <p>Sorry, the <strong>filename</strong> doesn't exist 😭.</p> 
+          <p>Make sure you don't have any typo or something.</p>
+        </div>`
+        // eslint-disable-next-line no-console
+        console.log(error)
         res.status(500).send(errorMsg)
-      }
-    })
-
-    fs.stat(thumbFilePath, (error)  => {
-      if (error) {
-        next()
       } else {
-        process.stdout.write('File already exist')
-        res.sendFile(path.resolve(thumbFilePath))
+        next()
       }
     })
   } else {
-    const errorMsg = 'Image can\'t be processed'
-    process.stdout.write(errorMsg)
-    res.send(errorMsg)
+    const errorMsg = `<div style="${inlineStyles}">
+      <p>Bummer! 😕 You need to provide at least the three required parameters, namely:</p> 
+      <ul style="list-style-type: none; margin: 0; padding: 0;">
+        <li>"filename"</li> 
+        <li>"width"</li>
+        <li>"height"</li>
+      </ul>
+    </div>`
+    // eslint-disable-next-line no-console
+    console.log(errorMsg)
+    res.status(500).send(errorMsg)
   }
 }
 
-export default imageProcessingMiddleware
+const checkThumbDirExists = async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+  try {
+    if (!fs.existsSync('thumb')) {
+      await fsPromises.mkdir('thumb')
+    }
+    
+    next()
+  } catch (error) {
+    const errorMsg = `<div style="${inlineStyles}">
+      <p>Whoops! Something went wrong, please try again.</p> 
+    </div>`
+    // eslint-disable-next-line no-console
+    console.log(error)
+    res.status(500).send(errorMsg)
+  }
+}
+
+const checkThumbImageExists = async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+  const filename = req.query.filename
+  const width = (req.query.width as unknown) as string
+  const height = (req.query.height as unknown) as string
+  const outputformat = req.query.outputformat || 'jpeg'
+  const thumbFilePath = `thumb/${filename}-w${width}-h${height}.${outputformat}`
+    
+  fs.stat(thumbFilePath, (error)  => {
+    if (error) { 
+      next()
+    } else {
+      res.status(200).sendFile(path.resolve(thumbFilePath))
+    }
+  })
+}
+
+export {
+  checkFullImageExists,
+  checkThumbDirExists,
+  checkThumbImageExists
+}
--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -2,12 +2,16 @@ import express from 'express'
 import sharp from 'sharp'
 import fs from 'fs'
 import path from 'path'
-import imageProcessingMiddleware from '../../middleware/images.middleware'
+import { checkFullImageExists, checkThumbDirExists, checkThumbImageExists } from '../../middleware/images.middleware'
 
+const imagesMiddleware = [checkFullImageExists, checkThumbDirExists, checkThumbImageExists]
 const images = express.Router()
+const inlineStyles = `position: absolute; left: 50%; 
+  top: 100px; transform: translateX(-50%);
+  font-family: sans-serif; text-align: center;
+  padding: 0 20px 10px 20px; background-color: #ffdd59; border-radius: 8px;`
 
-images.get('/', imageProcessingMiddleware, async (req, res): Promise<void> => {
-
+images.get('/', imagesMiddleware, async (req: express.Request, res: express.Response): Promise<void> => {
   const filename = req.query.filename
   const width = (req.query.width as unknown) as string
   const height = (req.query.height as unknown) as string
@@ -21,12 +25,18 @@ images.get('/', imageProcessingMiddleware, async (req, res): Promise<void> => {
       .toFormat(outputformat)
       .resize({ width: parseInt(width), height: parseInt(height) })
       .toBuffer()
+      
     fs.writeFileSync(thumbFilePath, image)
-    process.stdout.write('Image has been processed')
+    // eslint-disable-next-line no-console
+    console.log('✅ Image has been successfully processed! ✅')
     res.status(200).sendFile(path.resolve(thumbFilePath))
   } catch (error) {
-    process.stdout.write(error)
-    res.status(500)
+    const errorMsg = `<div style="${inlineStyles}">
+      <p>Image cannot be processed.</p>
+    </div>`
+    // eslint-disable-next-line no-console
+    console.log(error)
+    res.status(500).send(errorMsg)
   }
 })
 
