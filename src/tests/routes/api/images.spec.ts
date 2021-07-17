7f7d90289dbb87d1f045c5e2f672212efa958bf1
Diego Palacios <diegopalacios3@gmail.com>
2021-07-17 18:58:07 +0200

    chore: Add additional test and add to README

--- src/tests/routes/api/images.spec.ts
+++ src/tests/routes/api/images.spec.ts
@@ -8,14 +8,17 @@ const errorRoute = '/api/images?filename=foo&width=200&height=200'
 const routeImage200x200 = '/api/images?filename=laptop&width=200&height=200'
 const routeImage200x200Svg = '/api/images?filename=laptop&width=200&height=200&outputformat=svg'
 const routeImage300x300SourceJPEGOutputPNG = '/api/images?filename=laptop&width=300&height=300&sourceformat=jpeg&outputformat=png'
+const routeImage1200x1200SourcePNGOutputJPG = '/api/images?filename=lavender&width=1200&height=1200&sourceformat=png&outputformat=jpg'
 const routeImage300x300SourcePng = '/api/images?filename=laptop&width=200&height=200&sourceformat=png'
 const thumbPath = path.resolve('images/', 'thumb/', 'laptop-w200-h200.jpeg')
 const thumbPathPng = path.resolve('images/', 'thumb/', 'laptop-w300-h300.png')
+const thumbPathJpg = path.resolve('images/', 'thumb/', 'lavender-w1200-h1200.jpg')
 
 describe('🧪 /images resource ', () => {
   afterAll( async ()=> {
     await fs.unlink(thumbPath, (err) => { if (err) throw err })
     await fs.unlink(thumbPathPng, (err) => { if (err) throw err })
+    await fs.unlink(thumbPathJpg, (err) => { if (err) throw err })
   })
 
   it('Should return 500 error if filename does not exists', async () => {
@@ -42,6 +45,14 @@ describe('🧪 /images resource ', () => {
   it('Should generate image with sourceformat JPEG and outputformat PNG extenssions with status 201', async () => {
     const response = await request.get(routeImage300x300SourceJPEGOutputPNG)
     fs.access(thumbPathPng, fs.constants.F_OK, (err) => {
+      expect(err).toBe(null)
+    })
+    expect(response.status).toBe(201)
+  })
+
+  it('Should generate image with sourceformat PNG and outputformat JPG extenssions with status 201', async () => {
+    const response = await request.get(routeImage1200x1200SourcePNGOutputJPG)
+    fs.access(thumbPathJpg, fs.constants.F_OK, (err) => {
       expect(err).toBe(null)
     })
     expect(response.status).toBe(201)
