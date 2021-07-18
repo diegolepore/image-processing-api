7da09837c088f29fbd71f621138d239921212045
Diego Palacios <diegopalacios3@gmail.com>
2021-07-18 17:15:29 +0200

    test: Remove spaces

--- src/tests/routes/index.spec.ts
+++ src/tests/routes/index.spec.ts
@@ -4,10 +4,8 @@ import app from '../../index'
 const request = supertest(app)
 
 describe('🧪 Main /images route', () => {
-
   it('Should return 500 error when getting /images without corresponding query strings', async () => {
     const response = await request.get('/api/images')
     expect(response.status).toBe(500)
   })
-
 })
